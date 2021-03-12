e71c690f90fe704662a33b2656d73c91a94837f9
gjh's dad <1185259511@qq.com>
2021-03-12 11:04:51 +0800

    导入vant

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="app">
     <h1>黑马头条</h1>
+    <i class="toutiao toutiao-fenxiang"></i>
+    <br />
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
     <router-view />
   </div>
 </template>
